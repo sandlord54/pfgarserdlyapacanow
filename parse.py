4990e453a3f07dd7a4464d4ecb51a27cc2e59ede
sandlord54 <50053527+sandlord54@users.noreply.github.com>
2020-02-14 19:46:50 +0300

    Возможность вставлять ссылку при запуске проги, а не в коде

--- parse.py
+++ parse.py
@@ -7,16 +7,23 @@ def get_html(url):
 
 def get_data(html):
     soup = BeautifulSoup(html,'lxml')
-    h2 = soup.find('h1',{'class':'b-topic__title'})
+    h2 = soup.find('h1',{'class':'b-topic__title'})  #Заголовоу
     h1 = soup.find('div', {'class':'b-text clearfix js-topic__text'})
-    h3 = 'Заголовок статьи:\n'+h2.text+'\n' + 'Статья:\n' + h1.text
-    print(h3)
+    h4 = soup.find('p',{})
+    print(h4)
+    h3 = 'Заголовок статьи:\n'+h2.text+'\n' + 'Статья:\n' + h1.text+'\n'
+    #print(h3)
     f = open('text.txt','w',encoding='utf-8')
     f.write(h3)
     f.close()
+    if f.closed == True:
+        print('Записы выполнена!')
+
+    else:
+        print('Запись не выполнена!')
 
 def main():
-    html = get_html('https://lenta.ru/news/2020/02/10/orangutan/')
+    html = get_html(input('Введите URL сайта Lenta.ru'))
     get_data(html)
 
 if __name__ == '__main__':
